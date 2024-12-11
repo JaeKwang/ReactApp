9b4d6049bc7694e55b3b9496e231a0a00d1d00f9
Park JaeKwang <sowkdxkd12@gmail.com>
2024-12-11 11:32:24 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>안녕, 세상아!</h1>
+      <h2>Hello, world!</h2>
+      <p>VITE_API_KEY: {import.meta.env.VITE_API_KEY}</p>
+    </>
+  );
+}
+
+export default App;
